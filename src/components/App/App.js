6207b16aea3fcc3d548088d9848e3acfe90eb700
Ian Bruns <ian@rbruns.com>
2021-03-12 16:33:11 -0700

    added routes for landing and home

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import { Route, Switch } from 'react-router';
 import './App.css';
+import LandingPage from '../../routes/LandingPage/LandingPage';
+import PrivateRoute from '../../utilities/PrivateRoute';
 import NotFoundPage from '../../routes/NotFoundPage/NotFoundPage';
 import LoginPage from '../../routes/LoginPage/LoginPage';
 import RegistrationPage from '../../routes/RegistrationPage/RegistrationPage';
 import PublicOnlyRoute from '../../utilities/PublicOnlyRoute';
 import Header from '../Header/Header';
+import HomePage from '../../routes/HomePage/HomePage';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -29,6 +32,10 @@ function App() {
       <main>
         <h2>
           <Switch>
+            <Route
+              path exact='/'
+              component={LandingPage}
+            />
             <PublicOnlyRoute
               path={'/login'}
               component={LoginPage}
@@ -37,6 +44,10 @@ function App() {
             <PublicOnlyRoute
               path={'/register'}
               component={RegistrationPage}
+            />
+            <PrivateRoute
+              path={'/home'}
+              component={HomePage}
             />
             <Route
               component={NotFoundPage}
