71b7161c9e0d8362c0b8dfd19775f6afd46445ec
Ian Bruns <ian@rbruns.com>
2021-03-16 00:49:00 -0600

    added GamesRulesPage

---
+++ src/routes/GameRulesPage/GameRulesPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function GamesRulesPage(props) {
+    return (
+        <div className='GamesRulesPage'>
+            <p>Eyyyyy Steve!</p>
+        </div>
+    )
+}
