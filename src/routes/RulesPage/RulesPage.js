9769cf6f8d237e4dd5b288ae50859febc8f68f46
Ian Bruns <ian@rbruns.com>
2021-03-15 22:16:51 -0600

    called in Users games

--- src/routes/RulesPage/RulesPage.js
+++ src/routes/RulesPage/RulesPage.js
@@ -1,8 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import PlayPacketApiService from '../../services/playpacket-api-service';
 
 export default function RulesPage(props) {
     const [games, setGames] = useState([]);
+    useEffect(() => {
+        PlayPacketApiService.getUserGames()
+            .then(userGames => {
+                setGames(userGames);
+            })
+    })
+
+    const gameOptions = games.map(game => {
+        return (
+            <p>{game.game_name}</p>
+        )
+    })
+
     return (
-        <h2>RulesPage!</h2>
+        <>
+            <h2>RulesPage!</h2>
+            {gameOptions}
+        </>
     )
 }
