c072356cc51d614fd51be754021884d3db794b8d
Ian Bruns <ian@rbruns.com>
2021-03-15 22:13:17 -0600

    added API service

---
+++ src/services/playpacket-api-service.js
@@ -0,0 +1,19 @@
+import TokenService from './token-service';
+import config from '../config';
+
+const PlayPacketApiService = {
+    getUserGames() {
+        return fetch(`${config.API_ENDPOINT}/games`, {
+            headers: {
+                'authorization': `bearer ${TokenService.getAuthToken()}`,
+            },
+        })
+            .then(res =>
+                (!res.ok)
+                    ? res.json().then(e => Promise.reject(e))
+                    : res.json()
+            )
+    }
+}
+
+export default PlayPacketApiService;
