5ad953944519a571fda3eb04d39fbd0ac29d7ca3
Ian Bruns <ian@rbruns.com>
2021-03-17 01:10:46 -0600

    state management

--- src/routes/ResultsPage/ResultsPage.js
+++ src/routes/ResultsPage/ResultsPage.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 export default function ResultsPage(props) {
     const [name, setName] = useState('');
     const [rules, setRules] = useState([]);
+
     return (
         <p>Eh, Steve!</p>
     )
