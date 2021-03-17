2ede74cadb5f1f06a683e4d01b64d48360a92357
Ian Bruns <ian@rbruns.com>
2021-03-17 00:45:43 -0600

    change state management

--- src/routes/GameRulesPage/GameRulesPage.js
+++ src/routes/GameRulesPage/GameRulesPage.js
@@ -2,17 +2,29 @@ import React, { useState, useEffect } from 'react'
 import PlayPacketApiService from '../../services/playpacket-api-service';
 
 export default function GamesRulesPage(props) {
+    const [name, setName] = useState('')
     const [rules, setRules] = useState([]);
     useEffect(() => {
         PlayPacketApiService.getUserGameRules(props.match.params.gameId)
             .then(gameRules => {
                 setRules(gameRules);
+                setName(gameRules[0].game_name)
             })
-    }, []);
+    }, [props.match.params.gameId]);
+
+    const mapRules = rules.map(rule => {
+        return (
+            <div className='rule' key={rule.id}>
+                <p>{rule.rule_title}</p>
+                <p>{rule.rule_description}</p>
+            </div>
+        )
+    })
 
     return (
         <div className='GamesRulesPage'>
-            <p>Eyyyyy Steve!</p>
+            <h2>Rules For: {name}</h2>
+            {mapRules}
         </div>
     )
 }
