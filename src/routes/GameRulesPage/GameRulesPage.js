3d7ca04c60be7f9e57df3f3d8dbe75e160742f58
Ian Bruns <ian@rbruns.com>
2021-03-17 13:19:21 -0600

    added form

--- src/routes/GameRulesPage/GameRulesPage.js
+++ src/routes/GameRulesPage/GameRulesPage.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react'
 import PlayPacketApiService from '../../services/playpacket-api-service';
 
 export default function GamesRulesPage(props) {
+    const [loading, setLoading] = useState(false);
     const [adding, setAdding] = useState(false);
+
     const [name, setName] = useState('')
     const [rules, setRules] = useState([]);
     useEffect(() => {
@@ -22,6 +24,11 @@ export default function GamesRulesPage(props) {
         )
     })
 
+    function handleRuleSubmit(e) {
+        e.preventDefault();
+        console.log('boop');
+    }
+
     return (
         <div className='GamesRulesPage'>
             <h2>Rules For: {name}</h2>
@@ -32,7 +39,25 @@ export default function GamesRulesPage(props) {
             </button>
 
             {adding && (
-                <p>Adding!</p>
+                <form onSubmit={e => handleRuleSubmit(e)}>
+                    <div className='form-options'>
+                        <label htmlFor='rule_title'>Rule Title</label>
+                        <br />
+                        <input type='text' placeholder='Enter Rule Title' name='rule_title' />
+                    </div>
+                    <div className='form-options'>
+                        <label htmlFor='rule_description'>Rule Description*</label>
+                        <br />
+                        <input type='text' placeholder='Enter Rule Description'
+                            name='rule_description' required />
+                    </div>
+                    <button type="submit" className='myButton'
+                        disabled={loading}>
+                        {loading
+                            ? 'Hang Tight'
+                            : 'Add Rule'}
+                    </button>
+                </form>
             )}
         </div>
     )
