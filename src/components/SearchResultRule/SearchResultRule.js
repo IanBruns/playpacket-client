59269ba0dd2a3ef30a083361f09efc6771fe6a65
Ian Bruns <ian@rbruns.com>
2021-03-19 15:02:48 -0600

    added click event

--- src/components/SearchResultRule/SearchResultRule.js
+++ src/components/SearchResultRule/SearchResultRule.js
@@ -2,9 +2,13 @@ import React from 'react';
 import add from '../../images/plus.png';
 
 export default function SearchResultRule(props) {
+    const handleAddClicked = () => {
+        console.log('test')
+    }
     return (
         <div className='rule'>
-            <img src={add} alt='' height='25px' />
+            <img src={add} alt='' height='25px'
+                onClick={() => handleAddClicked()} />
             <p>{props.title}</p>
             <p>{props.description}</p>
         </div>
