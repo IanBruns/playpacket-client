01e0dffe67b82d58177b00607fdf7d3046331b67
Ian Bruns <ian@rbruns.com>
2021-03-18 21:16:28 -0600

    conditional rendering

--- src/components/UserRule/UserRule.js
+++ src/components/UserRule/UserRule.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import trash from '../../images/garbage.png'
 import edit from '../../images/edit.png'
 
 export default function UserRule(props) {
+    const [editing, setEditing] = useState(false)
+    const [title, setTitle] = useState(props.rule_title)
+    const [description, setDescription] = useState(props.rule_description)
+    const [editTitle, setEditTitle] = useState(title);
+    const [editDesc, setEditDesc] = useState(description);
+
+    function renderStaticItems() {
+        return (
+            <>
+                <p>{title}</p>
+                <p>{description}</p>
+            </>
+        )
+    }
+
+    function renderEditItems() {
+        return (
+            <>
+                <input type='text' placeholder={title} name='rule_title'
+                    value={editTitle} onChange={e => setEditTitle(e.target.value)} />
+                <br />
+                <input type='text' placeholder={description} name='rule_description'
+                    value={editDesc} onChange={e => setEditDesc(e.target.value)} />
+                <br />
+                <button>
+                    Post
+                </button>
+            </>
+        )
+    }
+
     return (
         <div className='rule'>
-            <p>{props.rule_title}</p>
-            <p>{props.rule_description}</p>
-            <img src={trash} alt='delete' height='20px'
-                onClick={() => props.handleDeleteClicked(props.id)} />
-            <img src={edit} alt='edit' height='20px' />
+            <div className='icons'>
+                <img src={trash} alt='delete' height='20px'
+                    onClick={() => props.handleDeleteClicked(props.id)} />
+                <img src={edit} alt='edit' height='20px'
+                    onClick={() => setEditing(!editing)} />
+            </div>
+
+            {(editing === true)
+                ? renderEditItems()
+                : renderStaticItems()}
         </div>
     )
 }
