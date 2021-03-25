7125b63e73e63084a2b9c611581e608a7e2de6ed
Ian Bruns <ian@rbruns.com>
2021-03-25 14:11:31 -0600

    committing last nights changes

--- src/components/RulesOptions/RulesOptions.js
+++ src/components/RulesOptions/RulesOptions.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './RulesOptions.css'
 
 export default function RulesOptions(props) {
     return (
         <Link to={`/Rules/${props.id}`}>
-            <button>
-                {props.name}
+            <button className='games-button'>
+                <span>{props.name}</span>
             </button>
         </Link>
     )
