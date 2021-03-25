48bd7fe007592bbc3f566ddcd0cf7a8f101f7dd4
Ian Bruns <ian@rbruns.com>
2021-03-24 20:31:51 -0600

    added style and substance to Landing Page

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './LandingPage.css'
+import dice from '../../images/perspective-dice-six-faces-four.png'
 
 export default function LandingPage(props) {
     return (
@@ -9,7 +11,47 @@ export default function LandingPage(props) {
                     <p>We gonna put a screenshot here</p>
                 </div>
                 <div className='right'>
-                    <p>right</p>
+                    <div className='bullet'>
+                        <img src={dice} alt='' height='30px' />
+                        <p>
+                            Did you know that it's against the rules of Uno to stack
+                            draw 2's on top of each other?
+                        </p>
+                    </div>
+                    <div className='bullet'>
+                        <img src={dice} alt='' height='30px' />
+                        <p>
+                            Crazy Right?!?  We think so
+                        </p>
+                    </div>
+                    <div className='bullet'>
+                        <img src={dice} alt='' height='30px' />
+                        <p>
+                            Here at Play Packet, you can create your OWN House Rules and share them
+                            with everyone else
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='container'>
+                <div className='left'>
+                    <div className='bullet'>
+                        <img src={dice} alt='' height='30px' />
+                        <p>
+                            So get logged in and show us the <span>REAL</span> way
+                            to play Settlers of Catan
+                        </p>
+                    </div>
+                    <div className='bullet'>
+                        <img src={dice} alt='' height='30px' />
+                        <p>
+                            Or whatever
+                        </p>
+                    </div>
+                </div>
+                <div className='right'>
+                    <p>Other Screenshot?</p>
                 </div>
             </div>
         </div>
