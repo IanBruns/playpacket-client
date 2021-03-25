3ec8ca9850dfbe75e1bcc8d3f1e1c23529a0b567
Ian Bruns <ian@rbruns.com>
2021-03-24 19:53:40 -0600

    adding to landing page

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 export default function LandingPage(props) {
     return (
-        <h2>Landing!</h2>
+        <div className='LandingPage'>
+            <h2>Welcome to Play Packet!</h2>
+            <div className='container'>
+                <div className='left'>
+                    <p>We gonna put a screenshot here</p>
+                </div>
+                <div className='right'>
+                    <p>right</p>
+                </div>
+            </div>
+        </div>
     )
 }
