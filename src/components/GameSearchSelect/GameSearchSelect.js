7efa8bfe98b4c87771fd1a72751e575c7bacca09
Ian Bruns <ian@rbruns.com>
2021-03-26 15:08:26 -0600

    changed buttons for game select search

--- src/components/GameSearchSelect/GameSearchSelect.js
+++ src/components/GameSearchSelect/GameSearchSelect.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './GameSearchSelect.css'
 
 export default function GameSearchSelect(props) {
     return (
         <div className='game-select'>
-            <p>{props.game_name}</p>
             <Link to={`/Results/${props.id}`}>
-                <button>
-                    Search!
+                <button className='search-game'>
+                    {props.game_name}
                 </button>
             </Link>
         </div>
