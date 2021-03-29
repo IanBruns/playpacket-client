0ecd52ed7531c8ed99020ecea2f069d3f02d5151
Ian Bruns <ian@rbruns.com>
2021-03-28 21:17:35 -0600

    all style implimented

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './LandingPage.css'
 import dice from '../../images/icons/perspective-dice-six-faces-four.png'
+import pizza from '../../images/photos/pizza.jpg';
+import scrabble from '../../images/photos/scrabble.jpg'
 import { Link } from 'react-router-dom';
 
 export default function LandingPage(props) {
@@ -9,7 +11,7 @@ export default function LandingPage(props) {
             <h2>Welcome to Play Packet!</h2>
             <div className='container'>
                 <div className='left'>
-                    <p>We gonna put a screenshot here</p>
+                    <img src={pizza} alt='' height='700px' />
                 </div>
                 <div className='right'>
                     <div className='bullet'>
@@ -32,30 +34,15 @@ export default function LandingPage(props) {
                             with everyone else
                         </p>
                     </div>
-                </div>
-            </div>
-
-            <div className='container'>
-                <div className='left'>
                     <div className='bullet'>
                         <img src={dice} alt='' height='30px' />
                         <p>
-                            So get logged in and show us the <span>REAL</span> way
-                            to play Settlers of Catan
+                            So Log in/get signed up, and show people the real way play settlers of catan
+                            or whatever
                         </p>
                     </div>
-                    <div className='bullet'>
-                        <img src={dice} alt='' height='30px' />
-                        <p>
-                            Or whatever
-                        </p>
-                    </div>
-                </div>
-                <div className='right'>
-                    <p>Other Screenshot?</p>
                 </div>
             </div>
-
             <div className='centered'>
                 <p>Ready to show Uno they can't boss us around?</p>
                 <Link to='/Register'>
